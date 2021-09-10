2f81e0d4c9592cc1ca6118b003612cb38be9cd50
Mary Marks <maryleemarks@gmail.com>
2021-09-10 14:34:01 -0700

    pass second bread test

--- Bakery.Tests/Models.Tests/BreadTests.cs
+++ Bakery.Tests/Models.Tests/BreadTests.cs
@@ -11,18 +11,18 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(5);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
-    // [TestMethod]
-    // public void GetBreadPrice_ReturnsBreadPrice_Int()
-    // {
-    //   int breadPrice = 5;
-    //   Bread newBread = new Bread(breadPrice);
-    //   int result = newBread.BreadPrice;
-    //   Assert.AreEqual(breadPrice, result);
-    // }
+     [TestMethod]
+     public void GetBreadPrice_ReturnsBreadPrice_Int()
+     {
+       int breadPrice = 5;
+       Bread newBread = new Bread(breadPrice);
+       int result = newBread.BreadPrice;
+       Assert.AreEqual(breadPrice, result);
+     }
 
   }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,24 +1,24 @@
-//  using System.Collections.Generic;
-//  using System;
-//  using Bakery.Models;
+ using System.Collections.Generic;
+ using System;
+ using Bakery.Models;
 
-//  namespace Bakery
-//  {
-//    public class Program
-//    {
+ namespace Bakery
+ {
+   public class Program
+   {
 
-//       public static void Main();
-//      {
-//        Console.WriteLine("Welcome to the Bakery!");
-    //   Console.WriteLine("Check out our Menu and Specials");
-    //   Console.WriteLine("Bread: $5 per loaf or buy 2 get 1 free!");
-    //   Console.WriteLine("Pastries: $2 per Pastry or 3 Pastries for $5!");
-    //   Console.WriteLine("How many loaves of bread would you like?");
-    //   string stringBreadAmount = Console.ReadLine();
-    //   int BreadAmount = int.Parse(stringBreadAmount);
-    //   Bread orderBread = newBread(breadAmount);
-    //   int finalPrice = orderBread.FinalbreadPrice();
-    //   Console.WriteLine("Your order is $" + finalPrice);
-  //     }
-  //   }
-  // }
+      public static void Main()
+     {
+      Console.WriteLine("Welcome to the Bakery!");
+      Console.WriteLine("Check out our Menu and Specials");
+      Console.WriteLine("Bread: $5 per loaf or buy 2 get 1 free!");
+      Console.WriteLine("Pastries: $2 per Pastry or 3 Pastries for $5!");
+      Console.WriteLine("How many loaves of bread would you like?");
+      string stringBreadAmount = Console.ReadLine();
+      int BreadAmount = int.Parse(stringBreadAmount);
+      Bread orderBread = new Bread(BreadAmount);
+      int finalPrice = orderBread.FinalBreadPrice();
+      Console.WriteLine("Your order is $" + finalPrice);
+      }
+    }
+   }
